0303147490946d67b7062c623b324a6d7a5e7ad3
David Daehne <david.daehne@mailbox.org>
2026-02-25 20:37:24 +0100

    create a slice with all features in order, pass needed vars

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/DerDaehne/gitops-playground/internal/config"
+	"github.com/DerDaehne/gitops-playground/internal/features"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.yaml.in/yaml/v2"
@@ -18,9 +20,26 @@ feature or config setting.
 
 you can turn everything on and off as you wish`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if viper.GetBool("application.debug") {
+
+		var globalConfig config.Config
+		if err := viper.Unmarshal(&globalConfig); err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR while unmarshaling config\n")
+		}
+		
+		println ("Starting Deployment ...")
+
+		if globalConfig.Application.Debug {
 			settings, _ := yaml.Marshal(viper.AllSettings())
 			fmt.Fprintf(os.Stdout, "Config: \n%s\n", settings)
+		}
+
+		// IMPORTANT: the order of features here defines the order in which they are installed
+		allFeatures := []features.Feature{
+			&features.ArgoCD{Config: globalConfig.Features.ArgoCD},
+		}
+
+		for _, f := range allFeatures {
+			if f.IsEnabled() { println(f.Name()) }
 		}
 	},
 }
