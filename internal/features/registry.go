22bced87aeb4640fc2e4baf9e52c8bfea4351000
David Daehne <david.daehne@cloudogu.com>
2026-03-06 14:03:05 +0100

    initial version of feature "registry" deployment

--- internal/features/registry.go
+++ internal/features/registry.go
@@ -1,9 +1,16 @@
 package features
 
-import "github.com/DerDaehne/gitops-playground/internal/config"
+import (
+	"github.com/DerDaehne/gitops-playground/internal/config"
+	"github.com/DerDaehne/gitops-playground/internal/helm"
+	"helm.sh/helm/v3/pkg/chartutil"
+	"k8s.io/client-go/kubernetes"
+)
 
 type Registry struct {
 	Config config.RegistryConfig
+	KubernetesClientSet kubernetes.Clientset
+	NamePrefix string
 }
 
 func (registry *Registry) Name() string {
@@ -20,5 +27,15 @@ func (registry *Registry) Validate() error {
 
 func (registry *Registry) Install() error {
 	println("Installing Registry")
+	var namespace string = registry.NamePrefix + "registry"
+
+	if registry.Config.Helm.Values == nil { registry.Config.Helm.Values = chartutil.Values{}}
+	registry.Config.Helm.Values["service"] = map[string]interface{}{
+		"nodePort": registry.Config.InternalPort,
+		"type": "NodePort",
+	}
+	
+	_, err := helm.DeployHelmChart(registry.Config.Helm, namespace, "registry")
+	if err != nil { return err }
 	return nil
 }
